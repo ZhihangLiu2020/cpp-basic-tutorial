d2a4f1345276fdfe7a0fdfcb5a32fe8ccf75de4e
zhihang liu <zhihang.liu@graviti.cn>
2022-01-19 14:18:30 +0800

    init commit

---
+++ .umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'C++ 教程',
+  mode: 'doc',
+  base: '/dist/',
+  // more config: https://d.umijs.org/config
+  description: 'C++基础教程',
+  logo: '/images/logo.png'
+});
---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+nav:
+  title: 简介
+---
+
+开始
+
+```cpp
+#include <iostream>
+
+int main() {
+  cout << "Hello, World!" << endl;
+}
+```
